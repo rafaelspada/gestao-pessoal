3b8b4f2cc59cba3976acc0caa236691e023caf63
rafaelspada <rafael_spada77@hotmail.com>
2025-06-12 17:53:29 -0300

    template para cadastros de despesas

--- gestao/views.py
+++ gestao/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class DespesaView(TemplateView):
     template_name = 'cadastro_despesa.html'
+
+    def post(self):
+        pass
--- gestaopessoal/urls.py
+++ gestaopessoal/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gestao/', include('gestao.urls')),
+    path('', include('gestao.urls')),
 ]
